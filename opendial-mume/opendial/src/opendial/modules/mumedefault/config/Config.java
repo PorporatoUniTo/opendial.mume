50d07ebdf4996eaf961332facde5c649f9e9b3d6
Aureliano Porporato <aureliano.porporato@edu.unito.it>
2019-06-06 12:27:12 +0200

    More testing done.

--- opendial-mume/opendial/src/opendial/modules/mumedefault/config/Config.java
+++ opendial-mume/opendial/src/opendial/modules/mumedefault/config/Config.java
@@ -5,7 +5,6 @@ import java.io.File;
 public class Config {
     private static final String s = File.separator;
     public static final String TINT_CONFIG = "configs" + s + "default-config.properties";
-    public static final String CORRECTION_CONFIG = "configs" + s + "correction-config.properties";
     public static final String LOG4J_CONFIG = "configs" + s + "log4j.conf";
     public static final String HEIDELTIME_CONFIG = "configs" + s + "config.props";
 
